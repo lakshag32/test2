037c14a65d86d9acf5b6b02257a72ed8155ab98c
lakshag32 <lakshag32@gmail.com>
2023-06-09 12:53:07 -0700

    inital commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,95 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
+
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.util.Units;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+  public static final int kDriverControllerPort = 0;
+    
+  public static final int kMotorID = 2;
+  
+  public static final int kContinuousCurrentLimit = 20;
+  public static final int kPeakCurrentLimit = 50;
+  public static final double kPeakCurrentDuration = 0.5;
+
+  public static final int kArmEncoderDistPerPulse = 2048; 
+
+  public static final double kP = 2;
+  public static final double kI = 0;
+  public static final double kD = 0.07;
+  public static final double kF = 0;
+
+  public static final boolean kUseTelemetry = false; 
+  /** the gravity compensation amount as a percent of full motor power, scaled by a cosine term (this value is the max FF) */
+  public static final double kGravityCompensation = 0.02;
+
+  public static final double kTolerance = 0.02;
+  public static final double kMotorPowerClamp = 0.8;
+
+  public static final TalonFXInvertType kMotorInvert = TalonFXInvertType.CounterClockwise;
+
+  public static final NeutralMode kNeutralMode = NeutralMode.Brake;
+
+  /** RoboRIO digital input port for the wrist absolute encoder */
+  public static final int kAbsEncoderPort = 7;
+  /** wrist absolute encoder offset (rotations). */
+  public static final double kEncoderOffset = 0.687; // Black tread: 0.687, blue tread: 0.704
+
+  public static final double kAutoTop = Units.rotationsToRadians(0.070);
+  public static final double kAutoMiddle = Units.rotationsToRadians(0.132);
+
+  public static final double kStowPos = 1.95;
+  public static final double kBottomNodeCubePos = Units.rotationsToRadians(0.170);
+  public static final double kMiddleNodeCubePos = 0.551; //Units.rotationsToRadians(0.170);
+  public static final double kTopNodeCubePos = 1.022;
+
+  public static final double kBottomNodeConePos = Units.rotationsToRadians(0.070);
+  public static final double kMiddleNodeConePos = Units.rotationsToRadians(0.070);
+  public static final double kTopNodeConePos = Units.rotationsToRadians(0.070);
+  
+  public static final double kIntakeConePos = 0.25; //blue tread: 0.1571; // 0.2199
+  public static final double kIntakeCubePos = 0.1;
+  public static final double kIntakeShelfPos = Units.rotationsToRadians(0.04);
+
+  public static final double kIntakeAltShelfPos = 1.35;
+
+  /** Wrist position angle minimum (radians) */
+  public static final double kMinPos = -0.16;
+  /** Wrist position angle maximum (radians) */
+  public static final double kMaxPos = 1.97;
+
+  //SIM
+  // to know how much the arm will move with a certain power, the sim needs to know the motor, gear ratio, MOI, and length
+  public static final DCMotor kGearBox = DCMotor.getFalcon500(1);
+
+  public static final double kGearRatio = (20.0/1.0) * (62.0/34.0) * (48.0/18.0);
+  
+  //calculate MOI using the center of gravity distance and weight
+  public static double kCOGWeight = Units.lbsToKilograms(7.3);
+  public static double kCOGDistance = Units.inchesToMeters(8.11);
+  
+  /** Wrist moment of inertia represents how hard it is to angularly accelerate (ie spin) something. */
+  public static final double kMomentOfInertia = kCOGWeight * kCOGDistance * kCOGDistance; // 0.1405
+
+
+  /** Length of the wrist joint (meters). Used in simulation and estimating accuracy. */
+  public static final double kLength = Units.inchesToMeters(16.1);
+}
+
+
---
+++ src/main/java/frc/robot/subsystems/WristSubsystem.java
@@ -0,0 +1,189 @@
+package frc.robot.subsystems;
+import frc.robot.Constants; 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.simulation.DutyCycleEncoderSim;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim; 
+
+public class WristSubsystem extends SubsystemBase {
+  private final WPI_TalonFX m_motor;
+  private final PIDController m_pid;
+
+  // unit tests want access to the DutyCycleEncoder and DutyCycleEncoderSim
+  protected final DutyCycleEncoder m_absEncoder;
+  protected DutyCycleEncoderSim m_absEncoderSim;
+
+  private double m_pidPower = 0;
+  private double m_power = 0;
+  private double m_lastPos = 0;
+
+  /** Physics Simulator for the wrist. takes in a motor voltage and calculates how much the arm will move. */
+  private final SingleJointedArmSim m_armSim;
+
+  public WristSubsystem(ShuffleboardTab wristTab) {
+    // configure the motor.
+    m_motor = new WPI_TalonFX(3); 
+
+    m_motor.setNeutralMode(Constants.kNeutralMode);
+    m_motor.setInverted(true); 
+    m_motor.enableVoltageCompensation(true);
+
+    // config deadband to be less, may be powering at small values to keep it up
+    m_motor.configNeutralDeadband(0.005);
+
+    // configure the encoder
+    m_absEncoder = new DutyCycleEncoder(Constants.kAbsEncoderPort);
+    // Cleaner encoder implementation
+    // offset to zero (arm horizontal)
+    m_absEncoder.setPositionOffset(Constants.kEncoderOffset);
+    // scale to radians and invert direction
+    m_absEncoder.setDistancePerRotation(-2.0 * Math.PI);
+
+    // make the PID controller
+    m_pid = new PIDController(Constants.kP, Constants.kI, Constants.kD);
+    // set the PID controller's tolerance
+    m_pid.setTolerance(Constants.kTolerance);
+
+    // go to the initial position
+    setSetpoint(Constants.kStowPos);
+
+    if (Constants.kUseTelemetry) {
+      setupShuffleboardTab(wristTab);
+    }
+
+    // if a simulation, set up the simulation resources
+    if (RobotBase.isSimulation()) {
+      // make the simulator
+      m_armSim = new SingleJointedArmSim(
+        Constants.kGearBox,
+        0.1,
+        0.1,
+        0.1,
+        0.1,
+        0.1, 
+        true
+      ); 
+
+      // make the encoder simulator
+      // this allows us to set the encoder during simulations...
+      m_absEncoderSim = new DutyCycleEncoderSim(m_absEncoder);
+    }
+  }
+
+
+  /**
+   * Set the Wrist's desired position.
+   * @param setpoint the desired arm position (in rotations)
+   */
+  public void setSetpoint(double setpoint) {
+    // set the PID integration error to zero.
+    m_pid.reset();
+    // set the PID desired position
+    m_pid.setSetpoint(setpoint);
+  }
+
+  @Override
+  public void periodic() {
+    // obtain the wrist position
+    double position = getAbsEncoderPos();
+
+    // calculate the PID power level
+    // for safety, clamp the setpoint to prevent tuning with SmartDashboard/Shuffleboard from commanding out of range
+    // This method continually changes the setpoint.
+    m_pidPower = m_pid.calculate(position, MathUtil.clamp(m_pid.getSetpoint(), Constants.kMinPos, Constants.kMaxPos));
+    
+    // calculate the value of kGravityCompensation
+    double feedforwardPower = Constants.kGravityCompensation * Math.cos(position);
+
+    // set the motor power
+    setMotorPower(m_pidPower + feedforwardPower);
+  }
+
+  /**
+   * Whether the wrist has reached its commanded position.
+   * @returns true when position has been reached
+   */
+  public boolean reachedSetpoint() {
+    return m_pid.atSetpoint();
+  }
+
+  /**
+   * Sets the motor power, clamping it and ensuring it will not activate below/above the min/max positions
+   */
+  private void setMotorPower(double power) {
+
+    m_power = MathUtil.clamp(power, -Constants.kMotorPowerClamp, Constants.kMotorPowerClamp);
+    
+    double pos = getAbsEncoderPos();
+
+    // double safety check incase encoder outputs weird values again
+    if (pos <= Constants.kMinPos && m_power < 0) {
+      m_power = 0;
+    }
+    if (pos >= Constants.kMaxPos && m_power > 0) {
+      m_power = 0;
+    }
+    
+    m_motor.set(m_power);
+  }
+
+  /**
+   * @return the absolute encoder position in rotations, zero being facing forward
+   */
+  public double getAbsEncoderPos() {
+    double pos = m_absEncoder.getDistance();
+    if (pos > Constants.kMaxPos || pos < Constants.kMinPos) {
+      pos = m_lastPos;
+    }
+    m_lastPos = pos;
+    return pos;
+  }
+
+  private void setupShuffleboardTab(ShuffleboardTab wristTab) {
+    wristTab.addNumber("Wrist Position", () -> getAbsEncoderPos());
+    wristTab.add("wrist PID", m_pid);
+    wristTab.addNumber("wrist power final", () -> m_power);
+    wristTab.addNumber("Wrist PID output", () -> m_pidPower);
+    wristTab.addNumber("Wrist Error", () -> m_pid.getSetpoint() - getAbsEncoderPos());
+    wristTab.addBoolean("At Setpoint", () -> reachedSetpoint());
+    wristTab.addNumber("Current Draw", () -> m_motor.getSupplyCurrent());
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // First, we set our "inputs" (the motor voltage)    
+    m_armSim.setInput(m_motor.get() * RobotController.getBatteryVoltage());
+
+    // update the physics simulation, telling it how much time has passed, and it will calculate how much the wrist has moved
+    m_armSim.update(0.02);
+
+    // set the distance to what we get from the sim
+    m_absEncoderSim.setDistance(m_armSim.getAngleRads());
+
+    // calculate the battery voltage based on the theoretical drawn amps.
+    // TODO: this is the wrong way to calculate battery voltage; it needs the sum of all currents.
+    // RoboRioSim.setVInVoltage(BatterySim.calculateDefaultBatteryLoadedVoltage(m_armSim.getCurrentDrawAmps()));
+
+    // update the drawing of the robot
+    
+    //DrawMechanism.getInstance().setWristAngle(m_armSim.getAngleRads());
+  }
+
+  /**
+   * Deallocate resources.
+   * <p>
+   * Test routines need to deallocate simulation resources.
+   */
+  public void close() {
+    m_motor.close();
+    m_absEncoder.close();
+  }
+}
